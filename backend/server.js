2a522f37869f1c91ac6240ae83ec7b83bc3707e8
andynguyen4267 <andynguyen4267@gmail.com>
2024-09-12 15:24:14 -0700

    port

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,9 @@ app.use('/api/auth', authRoutes);
 // Use the favorite routes
 app.use('/api/favorites', favoriteRoutes); // Handle favorites
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
