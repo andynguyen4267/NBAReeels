0f6bada4d9bf7f050fccc796f23e18aa049fdb60
andynguyen4267 <andynguyen4267@gmail.com>
2024-09-13 08:50:19 -0700

    sql render

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv').config(); // Load environment variables from .env file
 
+// "postgres://andynguyen:UCI.PEnguinman24.@localhost:5432/nbareels"
 // Create a new pool instance
 const pool = new Pool({
-    connectionString: "postgres://andynguyen:UCI.PEnguinman24.@localhost:10000/nbareels" // Use the DATABASE_URL from the .env file
+    connectionString: "postgresql://nbareels_psql_user:FuEe0aBSMJ9842o7cquQIMaPiPMMnM45@dpg-cri5t0rv2p9s73bkvoh0-a/nbareels_psql" // Use the DATABASE_URL from the .env file
 });
 
 module.exports = pool; // Export the pool for use in other files
